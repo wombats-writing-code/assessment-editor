ed8a534186ca858d335f5fc06296ccc447ac6f92
luwen-huang <huang.luwen@gmail.com>
2017-04-26 09:12:11 -0400

    new path for dev

--- src/reducers/utilities.js
+++ src/reducers/utilities.js
@@ -5,7 +5,7 @@ export const getHost = () => {
     return 'http://localhost:9999'
 
   } else if (location.host.indexOf('-dev') > -1) {
-    return 'http://open-ed-graph-dev.us-east-1.elasticbeanstalk.com/'
+    return 'http://open-ed-graph-dev.us-east-1.elasticbeanstalk.com'
 
   } else if (location.host.indexOf('mapping.mit.edu') > -1) {
     return 'http://open-ed-graph.aeizqnc7mw.us-east-1.elasticbeanstalk.com';
